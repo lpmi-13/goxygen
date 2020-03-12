e95fbfaa98efca8b4a20a8509348b85551bcd41f
Sasha Shpota <sashashpota@gmail.com>
2020-03-12 21:41:04 +0200

    PostgreSQL template

---
+++ templates/postgres.server/db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4"
+	"project-name/model"
+)
+
+type DB interface {
+	GetTechnologies() ([]*model.Technology, error)
+}
+
+type PostgresDB struct {
+	conn *pgx.Conn
+}
+
+func NewDB(conn *pgx.Conn) DB {
+	return PostgresDB{conn: conn}
+}
+
+func (d PostgresDB) GetTechnologies() ([]*model.Technology, error) {
+	rows, err := d.conn.Query(
+		context.Background(),
+		"select name, details from technologies",
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var tech []*model.Technology
+	for rows.Next() {
+		var name string
+		var details string
+		err = rows.Scan(&name, &details)
+		if err != nil {
+			return nil, err
+		}
+		tech = append(tech, &model.Technology{name, details})
+	}
+	return tech, nil
+}
